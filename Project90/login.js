e1fa5203503c3da145d0320c5d1d44f64d9e88a5
HenryW1227 <142699420+HenryW1227@users.noreply.github.com>
2024-04-06 11:13:56 +1000

    Add files via upload

---
+++ Project90/login.js
@@ -0,0 +1,15 @@
+function adduser()
+{
+    player1name = document.getElementById("player1_name_input").value;
+    player2name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1n",player1name);
+    localStorage.setItem("player2n",player2name);
+
+    window.location = "gamepage.html"
+
+}
+function namefind1()
+{
+    document.getElementById('Player1_name').innerHTML = player1name;
+}
